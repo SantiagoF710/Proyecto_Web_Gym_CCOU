de7573099de4deedcec6cc0692117874ca9c943f
SantiagoF710 <santifer710@hotmail.com>
2025-12-10 21:30:35 -0300

    Preparar app para Render

--- database/conexion.js
+++ database/conexion.js
@@ -1,7 +1,7 @@
 
 import mongoose from 'mongoose';
 
-const urlDatabase = 'mongodb+srv://santi71043:orFWgLK4NxwAb6Wd@cluster0.q9d8z8m.mongodb.net/GymCCOU?retryWrites=true&w=majority&appName=Cluster0';
+const urlDatabase = process.env.MONGODB_URI || 'mongodb+srv://santi71043:orFWgLK4NxwAb6Wd@cluster0.q9d8z8m.mongodb.net/GymCCOU?retryWrites=true&w=majority&appName=Cluster0';
 
 export const conectarDB = () => {
   return mongoose
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ import { manejarErrores } from './middlewares/manejarErrores.js';
 await conectarDB();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(cors());
